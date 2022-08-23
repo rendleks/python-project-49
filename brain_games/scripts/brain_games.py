fad472a7190f36c0c453b7ee9f2baf6010e0b3b2
rendleks <rendleks@bk.ru>
2022-08-23 22:27:28 +0300

    add files cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+from brain_games.scripts.cli import welcome_user
+
 def greeting():
     print("Welcome to the Brain Games!")
 
+
 def main():
     greeting()
+    welcome_user()
 
 if __name__ == "__main__":
     main()
